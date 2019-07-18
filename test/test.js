0176313ab16ed36e4c5db0c0b2dca7148d166c4a
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2019-07-18 21:58:09 +0200

    test: add default test

--- test/test.js
+++ test/test.js
@@ -1,27 +1,6 @@
-const { Struct, Types } = require("../");
+// Require Third-party Dependencies
+const avaTest = require("ava");
 
-const Schema = {
-    name: Types.char(10),
-    hp: Types.uint8()
-};
-const Player = new Struct(Schema);
-
-const buf = Player.encode({
-    name: "fraxken",
-    hp: 100
+avaTest("Husky passed Test", (assert) => {
+    assert.pass();
 });
-console.log(buf);
-
-const DATA = {
-    name: "fraxken",
-    hp: 100
-};
-
-const TIMES = 1000000;
-const then = Date.now();
-for (let i = 0; i < TIMES; i++) {
-    Player.encode(DATA);
-}
-const diff = Date.now() - then;
-console.log("Encoded %d objects in %d ms (%d enc/s)\n", TIMES, diff, (1000 * TIMES / diff).toFixed(0));
-
